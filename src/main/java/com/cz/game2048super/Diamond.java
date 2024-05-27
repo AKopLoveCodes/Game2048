4c9a24b2617a79c16b41b4540055ee205536f37c
cz <2082742762@qq.com>
2024-05-27 13:06:54 +0800

    1.1版本： 1 改动了方块颜色 2 改动了方块字体大小以适应字段长短 3 设计了2048方块的彩蛋

--- src/main/java/com/cz/game2048super/Diamond.java
+++ src/main/java/com/cz/game2048super/Diamond.java
@@ -2,6 +2,9 @@ package com.cz.game2048super;
 
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -53,34 +56,46 @@ public class Diamond {
             case 16:
                 rect.setFill(Color.web("#ff6666"));
                 text.setText("16");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),47));
                 break;
             case 32:
-                rect.setFill(Color.web("#cc3333"));
+                rect.setFill(Color.web("#cc3399"));
                 text.setText("32");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),47));
                 break;
             case 64:
-                rect.setFill(Color.web("#ff0033"));
+                rect.setFill(Color.web("#9933cc"));
                 text.setText("64");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),47));
                 break;
             case 128:
-                rect.setFill(Color.web("#cc0033"));
+                rect.setFill(Color.web("#0099ff"));
                 text.setText("128");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),45));
                 break;
             case 256:
-                rect.setFill(Color.web("#ffff99"));
+                rect.setFill(Color.web("#66cc66"));
                 text.setText("256");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),45));
                 break;
             case 512:
-                rect.setFill(Color.web("#ffff00"));
+                rect.setFill(Color.web("#66cc99"));
                 text.setText("512");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),45));
                 break;
             case 1024:
-                rect.setFill(Color.web("#ccffff"));
+                rect.setFill(Color.web("#ff99cc"));
                 text.setText("1024");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),43));
                 break;
             case 2048:
-                rect.setFill(Color.web("#0099cc"));
+                rect.setFill(new LinearGradient(
+                        0.0,0.0,1.0,0.0,true, CycleMethod.NO_CYCLE,
+                        new Stop(0.0,new Color( 0.14, 0.82, 0.95, 1.0)),
+                        new Stop(0.5,new Color( 0.84, 0.57, 0.98, 1.0)),
+                        new Stop(1.0,new Color( 1.0, 0.48, 0.52, 1.0))));
                 text.setText("2048");
+                text.setFont(Font.font("Berlin Sans FB Demi", FontWeight.findByName("Bold"),43));
                 break;
             default:
                 System.out.println("代码中出现错误：方块的值不在规定范围内");
